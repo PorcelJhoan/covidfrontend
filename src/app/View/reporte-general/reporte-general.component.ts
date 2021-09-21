c19540ace12b4e5683739342b81ac64d5167825d
Jhoan Porcel Peña <porcel.pena.jhoan@gmail.com>
2021-09-20 21:43:16 -0400

    probando mapbox

--- src/app/View/reporte-general/reporte-general.component.ts
+++ src/app/View/reporte-general/reporte-general.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {environment} from '../../../environments/environment';
+import * as mapboxgl from 'mapbox-gl'
 @Component({
   selector: 'app-reporte-general',
   templateUrl: './reporte-general.component.html',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReporteGeneralComponent implements OnInit {
 
+  map!: mapboxgl.Map;
   constructor() { }
 
   ngOnInit(): void {
+
+    (mapboxgl as any).accessToken =environment.mapboxkey;
+  
+  this.map= new mapboxgl.Map({
+  container: 'mapa-mapbox', // container ID
+  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  center: [-74.5, 40], // starting position
+  zoom: 9 // starting zoom
+  });
+   
+  // Add zoom and rotation controls to the map.
+  this.map.addControl(new mapboxgl.NavigationControl());
+  
+ this.CrearMrcador(-74.5, 40);
   }
 
+  CrearMrcador(lng:number, lat:number){
+    const marker = new mapboxgl.Marker({
+      draggable:true
+    })
+    .setLngLat([lng,lat])
+    .addTo(this.map)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReporteGeneralComponent } from './View/reporte-general/reporte-general.component';
+
+
 
 @NgModule({
   declarations: [
@@ -11,8 +12,10 @@ import { ReporteGeneralComponent } from './View/reporte-general/reporte-general.
     ReporteGeneralComponent
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
